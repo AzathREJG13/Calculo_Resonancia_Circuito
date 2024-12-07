5ff8004b0efd0af9b27131b1db12485f26342154
AzathREJG13 <rafaeleduardo130201@gmail.com>
2024-12-06 18:21:26 -0600

    Actualizacion

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Calculo de Resonancia de un Circuito (Python y C++ )
+
+Actualizacion, prueba del calculo de resonancia
--- calculo.cpp
+++ calculo.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <cmath> 
+
 using namespace std;
 
 int main() {
-    cout << "Hola, mundo desde C++!" << endl;
+    double L, C, f;
+
+    cout << "Ingrese la inductancia (en Henrios): ";
+    cin >> L;
+    cout << "Ingrese la capacitancia (en Faradios): ";
+    cin >> C;
+
+    if (L <= 0 || C <= 0) {
+        cout << "La inductancia y la capacitancia deben ser mayores a cero." << endl;
+        return 1; 
+    }
+
+    f = 1 / (2 * M_PI * sqrt(L * C));
+
+    cout << "La frecuencia de resonancia es: " << f << " Hz" << endl;
+
     return 0;
 }
